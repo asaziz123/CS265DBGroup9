540b8dc471837503cafe54fc9fbce64b25234918
Ulysses Yu <ulyu1898@gmail.com>
2018-03-02 17:36:05 -0600

    Added DWSA Data

---
+++ table_data.sql
@@ -0,0 +1,21 @@
+/*
+PWSIDs: 
+*/
+
+INSERT INTO LocationPolygon();
+
+DrinkingWaterServiceArea
+/*
+By: Jack
+Reviewed: Ulysses
+*/
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('TN1230000', 1000000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('TN1230001',100000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('TN1230002',200000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('TN1230003',300000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('CA1231111',300000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('TN1234567',100000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('CA1240000',10000000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('CA1240001', 200000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('CA1240002',100000);
+INSERT INTO DrinkingWaterServiceArea (PWSID, Population_Served) VALUES ('CA1240003',500000);
